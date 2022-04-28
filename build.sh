d0c89f95a79bd95ba51111ece27a7955117fce4d
Sneden Rebello <83523750+Sneden@users.noreply.github.com>
2022-04-28 19:38:19 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 
-#This script is used to run the first build from inside the server container.
-
-chmod +x run_server.sh
-cp -r /volumes/roving /root/
+#This script is used to run after the first erroneous build.
 
 cd /root/roving/examples
 chmod +x c-server
-chmod +x c-server_build
-cd ..
-./examples/c-server_build
 
 sed -i 's/zlib\.net/zlib\.net\/fossils/' /root/.cache/bazel/_bazel_root/*/external/com_google_protobuf/protobuf_deps.bzl
 
 sed -i 's/zlib\.net/zlib\.net\/fossils/' /root/.cache/bazel/_bazel_root/*/external/rules_proto/proto/private/dependencies.bzl
 
-./examples/c-server_build
+./examples/c-server
